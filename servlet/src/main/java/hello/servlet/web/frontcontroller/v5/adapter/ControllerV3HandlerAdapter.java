2d93c36ef8998de5d0a09ae563c9c5f445a54dc8
jkim9409 <jkimnyc2020@gmail.com>
2023-03-13 16:47:14 +0900

    controller annotation method
    controller annotation method

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -35,4 +35,3 @@ public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
     }
 }
 
-
---
+++ servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+// using the following annotation will do the same job as @Controller
+//@Component
+//@RequestMapping
+
+@Controller
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+
+}
